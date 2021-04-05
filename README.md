3298ded0fb9807e14894326934550b5cc36bd08f
pliniopereira10 <plinio.pereira10@gmail.com>
2021-04-05 20:00:26 -0300

    Adição do exercício cálculo do delta

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 4. :briefcase: [**Média dos 3 últimos salários. Convertendo String para Número.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/MediaSalarios.java)
 5. :1234: [**Cálculo expressão aritmética. Ordem precedência.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/OperadoresAritmeticos.java)
 6. :icecream::tv: [**Decisão de compras por meio operadores lógicos.Conversão Boolean para String.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/OperadoresLogicos.java)
+7. :arrow_up_small: [**Valor do delta, equação do 2º grau.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/CalculoDelta.java)
 7. :tv::ice_cream: [**Decisão de compra, utilizando operador ternário e lógico.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/OperLogicosETernarios.java)
 8. :1234: [**Calculadora de operações básicas, criado com operador ternário.**](https://github.com/pliniopereira10/resolucao-desafios-java/blob/main/desafios-no-curso/fundamentos/OperTernarioCalc.java)
 
---
+++ desafios-no-curso/fundamentos/CalculoDelta.java
@@ -0,0 +1,56 @@
+// Programa para cálculo da equação do 2º grau
+// Arquivo: AreaDoTriangulo.java
+
+package fundamentos;
+
+import java.util.Scanner;
+
+public class CalculoDelta {
+
+	public static void main(String[] args) {
+		
+		/* Criar um programa que resolve equações do segundo grau (ax2 + bx + c = 0)
+		 * utilizando a fórmula de Bhaskara.Encontre o delta
+		 */
+		
+		// Variáveis
+		int delta,
+			   valorA,
+			   valorB,
+			   valorC;
+		
+		String raizes;
+		
+		// Texto de Exibição
+				System.out.println(" *******************************");
+				System.out.println("    Cálculo Delta da Equação");
+				System.out.println(" *******************************");
+				
+		// Captura de dados
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print(" Informe valor de A: ");
+		valorA = teclado.nextInt();
+		
+		System.out.print(" Informe a altura valor de B: ");
+		valorB = teclado.nextInt();
+		
+		System.out.print(" Informe a altura valor de C: ");
+		valorC = teclado.nextInt();
+		
+		// Fórmula
+		delta = (int)Math.pow(valorB, 2) - (4 * valorA * valorC);
+		
+		raizes = delta < 0 ? "não possui raiz real." : "Opção Inválida.";
+		raizes = delta == 0 ? "possui uma única raiz real." : raizes;
+		raizes = delta > 0 ? "possui duas raízes reais." : raizes;
+		
+		System.out.printf(" Na equação %dx² + %dx + %d.", valorA, valorB, valorC);
+		System.out.printf(" O valor de delta é %d.%n", delta);
+		System.out.println(" Equação " + raizes);
+		
+		teclado.close(); // fim classe scanner
+		
+	} // fim método main
+
+} // fim classe AreaDoTriangulo
