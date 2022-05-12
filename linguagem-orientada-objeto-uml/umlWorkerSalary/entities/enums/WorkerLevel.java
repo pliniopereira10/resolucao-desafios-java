c4a837eb66f2cdc91f9597467d37736fe0c1e722
pliniopereira10 <plinio.pereira10@gmail.com>
2022-05-12 14:54:09 -0300

    Criação do pacote entities e entities.enums junto com a classe WorkerLevel

---
+++ linguagem-orientada-objeto-uml/umlWorkerSalary/entities/enums/WorkerLevel.java
@@ -0,0 +1,11 @@
+// Creation of the class WorkerLevel in order to identify the levels of employees
+
+package umlWorkerSalary.entities.enums;
+
+public enum WorkerLevel {
+
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
+	
+}
