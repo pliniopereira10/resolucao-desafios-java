5fb1f742d0d703c3d077b2fb1251d2f70ea091ed
pliniopereira10 <plinio.pereira10@gmail.com>
2022-05-12 15:20:02 -0300

    Update of the class Department

--- linguagem-orientada-objeto-uml/umlWorkerSalary/entities/Department.java
+++ linguagem-orientada-objeto-uml/umlWorkerSalary/entities/Department.java
@@ -1,5 +1,25 @@
+//	Creation of the class to identify the employee's department name  
+
 package umlWorkerSalary.entities;
 
 public class Department {
 
+	private String name;
+	
+	public Department() {
+		
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
