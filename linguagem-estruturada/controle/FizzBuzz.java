ac199db994913466f9b2f009a7bfeae70fd6358d
pliniopereira10 <plinio.pereira10@gmail.com>
2022-06-25 10:15:27 -0300

    If applied, will release version 1.5.0.Refactored source.

--- linguagem-estruturada/controle/FizzBuzz.java
+++ linguagem-estruturada/controle/FizzBuzz.java
@@ -1,12 +1,14 @@
 /*
  * Developer: pliniopereira10@gmail.com
  * Classname: FizzBuzz
- * Version: 1.0.0
+ * Version: 1.5.0
  * 
  * Programa tem como objetivo imprimir na saída do terminal a contagem de 1 até 100, posteriormente, as Strings:
  * "Fizz" para números múltiplos de 3
  * "Buzz" para números múltiplos de 5
- * "FizzBuzz" para números que são múltiplos de 3 e 5 ao mesmo tempo.
+ * "FizzBuzz" para números que são múltiplos de 3 e 5 ao mesmo tempo
+ * 
+ * Mudou da version 1.0.0 para 1.5.0 devido uma melhoria no código
  */
 
 package controle;
@@ -22,7 +24,7 @@ public class FizzBuzz {
 		int numberThree = 3;
 		int numberFive = 5;
 		
-		System.out.println("Printing numbers from 1 to 100:");
+		System.out.println("Printing numbers from 1 to 100:\n");
 		for (int i = numberOne; i <= numberOneHundred; i++) {
 			System.out.print(i + " ");
 			
@@ -33,40 +35,20 @@ public class FizzBuzz {
 		}
 		
 		System.out.println();
-		System.out.println("Printing numbers from 1 to 100 multiples of 3 prints Fizz:");
+		System.out.println("Printing numbers from 1 to 100:");
+		System.out.println("- numbers multiples of 3 prints Fizz.");
+		System.out.println("- numbers multiples of 5 prints Buzz.");
+		System.out.println("- numbers multiples of 3 and 5 prints FizzBuzz.\n");
 		for (int i = numberOne; i <= numberOneHundred; i++) {
-			if (i % numberThree == numberZero) {
-				System.out.print("Fizz ");
-			} else {
-				System.out.print(i + " ");
-			}
-			
-			if (i % numberTwenty == numberZero) {
-				System.out.println();
-			}
-			
-		}
-		
-		System.out.println();
-		System.out.println("Printing numbers from 1 to 100 multiples of 5 prints Fizz:");
-		for (int i = numberOne; i <= numberOneHundred; i++) {
-			if (i % numberFive == numberZero) {
-				System.out.print("Buzz ");
-			} else {
-				System.out.print(i + " ");
-			}
-			
-			if (i % numberTwenty == numberZero) {
-				System.out.println();
-			}
-			
-		}
-		
-		System.out.println();
-		System.out.println("Printing numbers from 1 to 100 multiples of 3 and 5 prints FizzBuzz:");
-		for (int i = numberOne; i <= numberOneHundred; i++) {
-			if (i % numberFive == numberZero && i % numberThree == numberZero) {
+			if (i % numberThree == numberZero && i % numberFive == numberZero) {
 				System.out.print("FizzBuzz ");
+				
+			} else if (i % numberThree == numberZero){
+				System.out.print("Fizz ");
+				
+			} else if (i % numberFive == numberZero) {
+				System.out.print("Buzz ");
+				
 			} else {
 				System.out.print(i + " ");
 			}
@@ -75,7 +57,7 @@ public class FizzBuzz {
 				System.out.println();
 			}
 			
-		}		 
+		}
 		 
 	}
 
